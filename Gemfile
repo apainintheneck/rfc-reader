a6cb89c89245aad0c96cb27d76cd0137909267ae
apainintheneck <apainintheneck@gmail.com>
2024-09-19 12:17:47 -0700

    Add test for recent command along with vcr gem

--- Gemfile
+++ Gemfile
@@ -13,3 +13,7 @@ gem "rspec-snapshot"
 gem "rubocop"
 gem "rubocop-rake"
 gem "rubocop-rspec"
+
+gem "vcr"
+
+gem "webmock"
---
+++ spec/lib/rfc_reader/recent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe RfcReader::Recent do
+  it "returns the list of recent RFCs" do
+    VCR.use_cassette("recent-list") do
+      expect(described_class.list).to match_snapshot("recent-list")
+    end
+  end
+end
